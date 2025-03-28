68aebb60b5b5c08693e76cea6ff213e10cb790f0
BMO-X <jesbe01@gmail.com>
2025-03-28 14:11:10 +0200

    Kansioiden lisäys
    Pages-kansio sivuja varten (esim.MainPage.xaml +.cx)
    
    Models-kansio luokkia varten (esim. Asiakas.cs)
    
    Services-kansio serviceluokkia varten (esim. AsiakasService.cs)

---
+++ HulluKyla/Pages/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using HulluKyla.Pages;
+namespace HulluKyla.Pages;
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+
