f273f06afc8c2d8569e3eae195e2ffe412b05137
BMO-X <jesbe01@gmail.com>
2025-04-01 14:07:05 +0300

    SqlService-luokan luonti
    Tietokantayhteyden luontiin tehty service

---
+++ HulluKyla/Services/SqlService.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HulluKyla.Services
+{
+    public static class SqlService
+    {
+        // Tietokannan "pääsytiedot"
+        private static readonly string connectionString =
+            "server=localhost;port=3307;user=root;password=Ruutti;database=vn;";
+
+        // Tietokantayhteyden luonti
+        public static MySqlConnection GetConnection() 
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+    }
+}
