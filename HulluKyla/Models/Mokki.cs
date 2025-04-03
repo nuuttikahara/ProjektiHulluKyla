f36c3d0074ff583d9793b2a9d64b90b783afa24d
finder <finder@occult.fi>
2025-04-03 14:40:38 +0300

    Worked on Mokki.
    Mokki is incomplete. The rest of the classes will follow soon.

--- HulluKyla/Models/Mokki.cs
+++ HulluKyla/Models/Mokki.cs
@@ -8,5 +8,92 @@ namespace HulluKyla.Models
 {
     internal class Mokki
     {
+        // Properties
+        private uint mokkiId;
+        private uint alueId;
+        private string postinro;
+        private string mokkiNimi;
+        private string katuosoite;
+        private double hinta;
+        private string kuvaus;
+        private int henkiloMaara;
+        private string varustelu;
+
+        // Static Properties
+        private readonly double DMAX = 100000000;
+
+        // Constructors
+        // Getters and Setters
+        public uint MokkiId
+        {
+            get => mokkiId;
+            private set { this.mokkiId = value; }
+        }
+
+        public uint AlueId
+        {
+            get => alueId;
+            set { this.alueId = value; }
+        }
+
+        public string Postinro
+        {
+            get => postinro;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Postinumero ei voi olla tyhjä tai null.");
+                else if (value.Trim().Length != 5)
+                    throw new ArgumentException("Postinumeron täytyy olla 5 merkkiä pitkä.");
+
+                postinro = value.Trim();
+            }
+        }
+
+        public string MokkiNimi
+        {
+            get => mokkiNimi;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    this.mokkiNimi = null;
+                else if (value.Trim().Length > 45)
+                    throw new ArgumentException("Mökin nimen maksimipituus on 45 merkkiä.");
+                else
+                    this.mokkiNimi = value.Trim();
+            }
+        }
+
+        public string Katuosoite
+        {
+            get => katuosoite;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    this.katuosoite = null;
+                else if (value.Trim().Length > 45)
+                    throw new ArgumentException("Katuosoitteen maksimipituus on 45 merkkiä.");
+                else
+                    this.katuosoite = value.Trim();
+            }
+        }
+
+        public double Hinta
+        {
+            get => hinta;
+            set
+            {
+                if (value >= DMAX)
+                    throw new ArgumentException(
+                        "Hinta ei voi olla yhtä suuri tai suurempi kuin " + DMAX + "."
+                    );
+                else if (value < 0)
+                    throw new ArgumentException("Hinta ei voi olla alle 0.");
+                else
+                    this.hinta = value;
+            }
+        }
+
+        // Methods
     }
 }
