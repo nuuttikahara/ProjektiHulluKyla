be9bda8f9cb4f6ea9f34871fbe4565e98c2181b7
JayBdrummer <j.laplanders@gmail.com>
2025-05-02 16:46:45 +0300

    VarausListaPage
    sivun toiminta päivitetty

--- HulluKyla/Pages/VarausListaPage.xaml.cs
+++ HulluKyla/Pages/VarausListaPage.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.Maui.Controls;
 using HulluKyla.Services;
 using HulluKyla.Models;
 using System.Collections.ObjectModel;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace HulluKyla.Pages;
 
@@ -10,6 +11,8 @@ public partial class VarausListaPage : ContentPage {
     private List<Asiakas> kaikkiAsiakkaat;
     private Asiakas? valittuAsiakas;
     private Varaus? valittuVaraus;
+    private VarauksenPalvelu? varauksenPalvelu;
+    private Palvelu? lisattavaPalvelu;
 
     public VarausListaPage() {
 
@@ -21,6 +24,13 @@ public partial class VarausListaPage : ContentPage {
         base.OnAppearing();
         kaikkiAsiakkaat = AsiakasService.HaeKaikki();
         AsiakasListaView.ItemsSource=kaikkiAsiakkaat;
+        varauksenPalvelu = null;
+        lisattavaPalvelu = null;
+        valittuVaraus = null;
+        AsiakkaanVaraukset.ItemsSource = null;
+        VarauksenPalvelut.ItemsSource = null;
+        KaikkiPalvelut.ItemsSource = null;
+        LkmEntry.Text = "";
     }
 
     
@@ -51,9 +61,77 @@ public partial class VarausListaPage : ContentPage {
     private async void VarausSelected(object sender, SelectionChangedEventArgs e) {
         valittuVaraus = e.CurrentSelection.FirstOrDefault() as Varaus;
         VarauksenPalvelut.ItemsSource = VarauksenPalvelutService.HaeVarauksenPalvelut(valittuVaraus.VarausId);
+        KaikkiPalvelut.ItemsSource = PalveluService.HaeAlueenPalvelut(valittuVaraus.Mokki.AlueId);
     }
     private async void VarauksenPalveluSelected(object sender, SelectionChangedEventArgs e) {
+        varauksenPalvelu = e.CurrentSelection.FirstOrDefault() as VarauksenPalvelu;
+    }
 
+    private async void KaikkiPalvelutSelected(object sender, SelectionChangedEventArgs e) {
+        lisattavaPalvelu = e.CurrentSelection.FirstOrDefault() as Palvelu;
+    }
+
+    private async void PoistaVaraus_Clicked(object sender, EventArgs e) {
+        if (valittuVaraus == null) {
+            await DisplayAlert("Virhe", "Valitse ensin varaus", "OK");
+            return;
+        }
+        try {
+            VarausService.Poista(valittuVaraus.VarausId);
+            await DisplayAlert("Poista", "Varauksen poisto onnistui", "OK");
+            PaivitaSivu();
+
+        } catch (Exception ex) {
+            await DisplayAlert("Virhe", $"Varausta poistettaessa tapahtui virhe: {ex.Message}", "OK");
+
+        }
+    }
+
+    private async void PoistaPalvelu_Clicked(object sender, EventArgs e) {
+        if (valittuVaraus == null || varauksenPalvelu == null) {
+            await DisplayAlert("Virhe", "Valitse ensin varaus ja poistettava palvelu", "OK");
+            return;
+        }
+        try {
+            VarauksenPalvelutService.PoistaPalveluVarauksesta(valittuVaraus.VarausId, varauksenPalvelu.Palvelu.PalveluId);
+            await DisplayAlert("Poista", "Palvelun poisto varauksesta onnistui", "OK");
+            PaivitaSivu();
+
+        } catch (Exception ex) {
+            await DisplayAlert("Virhe", $"Palvelua poistettaessa tapahtui virhe: {ex.Message}", "OK");
+        }
+    }
+
+    private async void LisaaPalvelu_Clicked(object sender, EventArgs e) {
+        if (valittuVaraus == null || lisattavaPalvelu == null) {
+            await DisplayAlert("Virhe", "Valitse ensin varaus ja lis‰tt‰v‰ palvelu", "OK");
+            return;
+        }
+        if (int.TryParse(LkmEntry.Text, out int lkm)) {
+
+        } else {
+            await DisplayAlert("Virhe", "Virheellinen kappalem‰‰r‰", "OK");
+            return;
+        }
+        try {
+            VarauksenPalvelutService.LisaaPalveluVaraukseen(valittuVaraus.VarausId, lisattavaPalvelu.PalveluId, lkm);
+            await DisplayAlert("Lis‰‰", "Palvelun lis‰ys varaukseen onnistui", "OK");
+            PaivitaSivu();
+
+        } catch (Exception ex){
+            await DisplayAlert("Virhe", $"Palvelua lis‰tt‰ess‰ tapahtui virhe: {ex.Message}", "OK");
+        }
+    }
+
+    private void PaivitaSivu() {
+        varauksenPalvelu = null;
+        lisattavaPalvelu = null;
+        valittuVaraus = null;
+        AsiakkaanVaraukset.ItemsSource = null;
+        VarauksenPalvelut.ItemsSource = null;
+        KaikkiPalvelut.ItemsSource = null;
+        LkmEntry.Text = "";
+        AsiakkaanVaraukset.ItemsSource = VarausService.HaeAsiakkaanVaraukset(valittuAsiakas.AsiakasId);
     }
 
 
